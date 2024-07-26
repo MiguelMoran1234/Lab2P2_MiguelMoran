97636e1dbb746a16f466379311db3e8b5400b6ab
Hdbygdla <131492426+MiguelMoran1234@users.noreply.github.com>
2024-07-26 16:18:34 -0600

    Add files via upload

---
+++ Lab2P2_MiguelMora/src/Herramienta.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author flash
+ */
+public class Herramienta {
+    String Nombre;
+    String Material;
+
+    public Herramienta(String Nombre, String Material) {
+        this.Nombre = Nombre;
+        this.Material = Material;
+    }
+    
+
+}
